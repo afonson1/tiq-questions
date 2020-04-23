67c0ab2744d1351ca317cfff60664a2cfc559703
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-23 20:20:46 -0300

    Create Banca.java

---
+++ src/models/Banca.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Banca {
+	private String banca;
+	
+	public Banca (String banca){
+		this.banca = banca;
+	}
+
+	public String getBanca() {
+		return banca;
+	}
+
+	public void setBanca(String banca) {
+		this.banca = banca;
+	}
+}
