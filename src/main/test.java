8f073f3e6eefb1ab87232390efb2239e5c18d915
Afonso Serafim Jacinto <aphonson1@gmail.com>
2020-04-26 15:58:03 -0300

    Update test.java

--- src/main/test.java
+++ src/main/test.java
@@ -13,11 +13,12 @@ public class test {
 		Prova provaTeste = new Prova(2020, bancaCespe, "BNB", "Analista");
 		
 		//Criando questões
-		Question questao = new Question(provaTeste, "Acerca de conceitos e disciplinas da engenharia de software, julgue o item que se segue.", "No modelo de desenvolvimento de software em cascata, a abordagem é orientada ao risco e as tarefas são organizadas nos seguintes ciclos: determinar objetivos, identificar e resolver riscos, desenvolver e testar, e planejar a próxima iteração.", null, null, "Gabarito", null, null);
+		Question questao = new Question("CertoOuErrado", provaTeste, "Introdução da Questão", "Enunciado da questão", 'C');
 		questao.imprimeQuestão();
 		System.out.println(Question.getId());
 		
-		Question questao2 = new Question(provaTeste, "Acerca de conceitos e disciplinas da engenharia de software, julgue o item que se segue.", "No modelo de desenvolvimento de software em cascata, a abordagem é orientada ao risco e as tarefas são organizadas nos seguintes ciclos: determinar objetivos, identificar e resolver riscos, desenvolver e testar, e planejar a próxima iteração.", null, null, "Gabarito", null, null);
+		Question questao2 = new Question("MultiplaEscolha", provaTeste, "Acerca de conceitos e disciplinas da engenharia de software, julgue o item que se segue.",
+				"No modelo de desenvolvimento de software em cascata, a abordagem é orientada ao risco e as tarefas são organizadas nos seguintes ciclos: determinar objetivos, identificar e resolver riscos, desenvolver e testar, e planejar a próxima iteração.", "Letra A", null, "Gabarito", null, null, 'C');
 		questao2.imprimeQuestão();
 		System.out.println(Question.getId());
 		System.out.println(questao.getIdQuestao());
